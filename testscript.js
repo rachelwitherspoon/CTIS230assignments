f7a7a94bfbf7fd3867abb15a2de161edee17e6ed
rachelwitherspoon <64097394+rachelwitherspoon@users.noreply.github.com>
2020-05-06 11:43:52 -0400

    add image transformation

--- testscript.js
+++ testscript.js
@@ -1,4 +1,4 @@
-myitem = document.getElementById("firsttest"); 
+myitem = document.getElementById("firsttest");
 //Identify item
 
 myitem.addEventListener("click", onClick);
@@ -17,18 +17,18 @@ function onMouseOut() {
     myitem.style.backgroundColor = "";
 }
 //When the cursor is moved the text returns to it's original format
-thebutton = document.getElementById("thebutton");  
+thebutton = document.getElementById("thebutton");
 otheritem = document.getElementById("buttontest");
 thebutton.addEventListener("click", onAnotherButtonClick);
-function onAnotherButtonClick() {  
+function onAnotherButtonClick() {
     otheritem.style.color = "mistyrose";
     otheritem.style.backgroundColor = "midnightblue";
 }
 
-anotherbutton = document.getElementById("anotherbutton");  
+anotherbutton = document.getElementById("anotherbutton");
 anotheritem = document.getElementById("anotherbuttontest");
 anotherbutton.addEventListener("click", onButtonClick);
-function onButtonClick() {  
+function onButtonClick() {
     anotheritem.style.color = "mistyrose";
     anotheritem.style.backgroundColor = "midnightblue";
 }
@@ -44,9 +44,39 @@ function onMouseOut() {
     header.style.color = "";
 }
 
-textentry = document.getElementById("myinput");  
+textentry = document.getElementById("myinput");
 textentry.addEventListener("change", onChange);
-function onChange() {  
-    newtext = myinput.value;  
-    otheritem.innerHTML = newtext;  
+function onChange() {
+    newtext = myinput.value;
+    otheritem.innerHTML = newtext;
+}
+
+theImgDiv = document.getElementById("logo");
+theImage = document.getElementById("logoimage");
+theBigImage = document.getElementById("largeview");
+cardShown = false;
+
+theImgDiv.addEventListener("click", makeBigImage);
+//theBigImage.addEventListener("click", hideBigImage);
+theBigImage.innerHTML = "";
+
+function makeBigImage() {
+    if (theBigImage.innerHTML == "") {
+        bigimage = document.createElement("img");
+        bigimage.src = "Images/logo.jpg";
+        theBigImage.appendChild(bigimage);
+        theBigImage.classList.remove("dontshow");
+
+        if (theImgDiv.style.position == "") {
+            theImgDiv.style.position = "absolute";
+            theImgDiv.style.top = "300";
+            theImgDiv.style.left = "300";
+            theImage.style.width = "40%";
+            theImage.src = "Images/ecg.jpg";
+        }
+        else {
+            theImgDiv.style.position = "";
+            theImage.style.width = "100";
+        }
+    }
 }
